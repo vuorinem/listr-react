e3d377609e782f341fdca5f678d29d2de2e4f4e6
Mikko Vuorinen <Mikko.Vuorinen@sitekit.net>
2020-03-03 16:34:13 +0000

    Ad test values to the list API

--- src/list/list-api.ts
+++ src/list/list-api.ts
@@ -1,4 +1,4 @@
-type ItemData = {
+export type ItemData = {
     label: string;
     link?: string;
     count: number;
@@ -6,15 +6,37 @@ type ItemData = {
     myReservedCount: number;
 }
 
-type ListData = {
+export type ListData = {
     name: string;
     items: ItemData[];
 }
 
 export function getList(name: string): Promise<ListData> {
-    // TODO
-    return Promise.resolve({
+    // TODO: Make the API call
+    return Promise.resolve(<ListData>{
         name: name,
-        items: [],
+        items: [
+            {
+                label: 'First item',
+                count: 1,
+                link: 'https://www.google.com',
+                reservedCount: 0,
+                myReservedCount: 0,
+            },
+            {
+                label: 'Second item',
+                count: 1,
+                link: 'https://www.google.com',
+                reservedCount: 1,
+                myReservedCount: 0,
+            },
+            {
+                label: 'Third item',
+                count: 1,
+                link: 'https://www.google.com',
+                reservedCount: 1,
+                myReservedCount: 1,
+            },
+        ],
     });
 }
