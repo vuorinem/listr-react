6dc5d3385011cb1a4a37b55e486a23e472f4e2a5
Mikko Vuorinen <vuorinem@gmail.com>
2020-03-22 17:29:27 +0000

    Use hooks to load list data

--- src/list/list.tsx
+++ src/list/list.tsx
@@ -1,27 +1,33 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Item } from "./item";
+import { ListData, getList } from "./list-api";
 
 type ListProps = {
     name: string;
 }
-const TestItems = [
-    'First item',
-    'Second item',
-    'Third item',
-];
 
 export const List: FunctionComponent<ListProps> = (props) => {
     const handleReserve = (item: string) => {
         alert(`Reserved '${item}'`);
     }
 
+    const [list, setList] = useState<ListData | null>(null);
+
+    useEffect(() => {
+        const loadList = async () => {
+            setList(await getList(props.name));
+        };
+
+        loadList();
+    }, [props.name]);
+
     return (
         <div className="listr-list">
             <h1>{props.name}</h1>
-            {TestItems.map((item, index) => (
+            {list?.items.map((item, index) => (
                 <Item
                     key={index}
-                    label={item}
+                    label={item.label}
                     onReserve={(item) => handleReserve(item)}
                 />
             ))}
