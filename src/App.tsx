3e6265dd6708b477ad7be7f244e5db7b8d5fe14d
Mikko Vuorinen <vuorinem@gmail.com>
2020-04-04 23:39:13 +0100

    Allow setting list name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { List } from './list/list';
 
 function App() {
+  const [listName, setListName] = useState('My List');
+
   return (
     <div className="App">
-      <List name="My List" />
+      <input value={listName} onChange={e => setListName(e.target.value)} />
+      {listName ? <List name={listName} /> : null}
     </div>
   );
 }
